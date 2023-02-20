2a59dbd8c431cae3f0534ab26ab0907f791886c5
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-21 01:05:02 +0300

    add task Cubing

---
+++ Cubing/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Cubing(PutNumber("Введите число : "));
+
+void Cubing(int number)
+{
+  for (int i = 1; i <= number; i++)
+  {
+    System.Console.Write(Math.Pow(i, 3) + " ");
+  }
+}
+
+int PutNumber(string greeting)
+{
+  System.Console.Write(greeting);
+  int number = Convert.ToInt32(Console.ReadLine());
+  return number;
+}
