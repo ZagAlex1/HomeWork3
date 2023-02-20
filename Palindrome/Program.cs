cf9181e321eba1307e0afb6c897ef49028211aaf
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-20 22:01:13 +0300

    add task palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+
+PalicOrNo(PutNumber("Введите пятизначное число : "));
+
+char[] PutNumber(string greeting)
+{
+  System.Console.Write(greeting);
+  while (true)
+  {
+    int number = Convert.ToInt32(Console.ReadLine());
+    char[] array = number.ToString().ToCharArray();
+    if (array.Length != 5)
+      System.Console.WriteLine("Введите правильное число!!!");
+    else
+      return array;
+  }
+}
+
+void PalicOrNo(char[] array)
+{
+  if (array[0] == array[array.Length - 1] && array[1] == array[array.Length - 2])
+    System.Console.WriteLine("Число палиндром");
+  else
+    System.Console.WriteLine("Число не палиндром");
+}
+
+
+
+
